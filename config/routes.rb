5a4ed11766786c3926a5a4e023e272ea61c674d3
bruna romeiro <bruna_romeiro@yahoo.com.br>
2020-03-19 23:05:06 -0300

    Add Book routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'books/show'
+  get 'books/index'
+  get 'books/new'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :book, only: [:show, :index, :new, :create]
 end
