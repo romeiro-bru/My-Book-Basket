eb0c8de5b23e5202b19d121334338c58f7090768
bruna romeiro <bruna_romeiro@yahoo.com.br>
2020-03-19 22:26:32 -0300

    Add validation, book and review - title and content presence

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,6 @@ class Book < ApplicationRecord
   belongs_to :user
   has_many :reviews
   has_many :lists, through: :users
+  validates :title, :photo, presence: true
+  validates :title, uniqueness: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,4 @@
 class Review < ApplicationRecord
   belongs_to :book
+  validates :content, presence: true
 end
