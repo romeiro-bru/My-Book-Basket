a0f8eb2ffbba7e0c0b9add774545fa9c91887926
bruna romeiro <bruna_romeiro@yahoo.com.br>
2020-03-29 00:55:46 -0300

    Create review show

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   root to: 'books#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :books, only: [:index, :show, :new, :create] do
-    resources :reviews, only: [:new, :create]
+    resources :reviews, only: [:index, :new, :create]
   end
 end
