53cfcd395632714cd31fd4d849808560a2568253
bruna romeiro <bruna_romeiro@yahoo.com.br>
2020-03-29 21:51:03 -0300

    Fix review-new page, Add Footer

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   resources :books, only: [:index, :show, :new, :create] do
     resources :reviews, only: [:index, :new, :create]
   end
+  resources :reviews, only: [:show]
 end
