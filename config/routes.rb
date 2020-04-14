9ae584efa48a97516a3417a24597f45bbcbcdcb0
bruna romeiro <bruna_romeiro@yahoo.com.br>
2020-04-14 13:10:24 -0300

    Fix route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
   devise_for :users
   root to: 'books#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
