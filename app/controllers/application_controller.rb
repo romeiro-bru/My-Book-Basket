954cf18604dae28d41064163b80863d42da053ca
bruna romeiro <bruna_romeiro@yahoo.com.br>
2020-04-14 22:12:04 -0300

    Fix before action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,3 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  skip_before_action :authenticate_user!, :only => [:index, :show]
-  skip_authorize_resource :only => [:index, :show]
-
 end
