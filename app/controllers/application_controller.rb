8a68b6e54044b935f7674cc161d7427dbb9f44db
bruna romeiro <bruna_romeiro@yahoo.com.br>
2020-04-14 21:30:06 -0300

    Skip before action login - only index and show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  skip_before_action :authenticate_user!, :only => [:index, :show]
+  skip_authorize_resource :only => [:index, :show]
+
 end
