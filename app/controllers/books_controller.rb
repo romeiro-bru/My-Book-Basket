8110085a8b98bef555ab63354e7ff2247921990a
bruna romeiro <bruna_romeiro@yahoo.com.br>
2020-04-15 22:55:48 -0300

    Edit Book - routes and controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,6 +9,17 @@ class BooksController < ApplicationController
     @books = Book.all
   end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book)
+
+  end
+
   def new
     @book = Book.new
   end
